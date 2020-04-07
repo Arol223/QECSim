c71be81f5e488047c34d8071618fc43ea3cc9050
Arvid Rolander <arvid.rolander@gmail.com>
2020-04-07 11:33:50 +0200

    "Added a few comments"

--- Operations/QuantumOperations/QuantumOperation.m
+++ Operations/QuantumOperations/QuantumOperation.m
@@ -1,5 +1,6 @@
 classdef QuantumOperation < handle
-    
+    % Basic class for doing quantum operations on single bits. Operation
+    % elements are matrices which act on one bit at a time.
     properties (Abstract)
         operation_elements
         element_weights
@@ -11,7 +12,8 @@ classdef QuantumOperation < handle
         
         end
         
-        function op = nbit_op_element(obj, element_number, targets, tot_bits)
+        function op = nbit_op_element(obj, tot_bits, element_number,...
+                                        targets)                
             % Creates a matrix for one of the operation elements acting on
             % bits targets. Example: 3-bit system, operation element is
             % pauli X, and we want to apply it to bits 2 & 3. The matrix
@@ -36,15 +38,16 @@ classdef QuantumOperation < handle
             end
             if isa(nstate, 'NbitState')
                 rho_i = nstate.rho;
-                rho = zeros(size(rho_i));
                 tot_bits = nstate.nbits;
             else
                 rho_i = nstate;
-                rho = rho_i;
                 tot_bits = log2(size(nstate,1));
             end
-            for i = 1:size(obj.operation_elements,3)
-                op = obj.nbit_noise_element(i, targets, tot_bits);
+            op = obj.nbit_op_element(1, targets, tot_bits);
+            rho = op*rho_i*op';
+                
+            for i = 2:size(obj.operation_elements,3)
+                op = obj.nbit_op_element(i, targets, tot_bits);
                 rho = rho + op*rho_i*op';
             end
         end
