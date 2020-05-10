b8512b4074f342c4fedaf84fa895ec3ffa88932e
Arvid Rolander <arvid.rolander@gmail.com>
2020-05-10 22:00:56 +0200

    Added comparison between different tolerances

--- Simulations/CodeTiming.m
+++ Simulations/CodeTiming.m
@@ -40,7 +40,7 @@ stdev(3) = std(times);
 [cnot,cz,~,~,zgate,hadgate] = MakeGates(150e-6,1e-3,1e-6,0,0);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [rtot,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
     times(i) = toc;
 end
 t_mean(4) = mean(times);
@@ -50,53 +50,58 @@ stdev(4) = std(times);
 [cnot,cz,~,~,zgate,hadgate] = MakeGates(150e-6,150e-6,5e-6,1e-5,0);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [r,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    
     times(i) = toc;
 end
 t_tol(1) = mean(times);
 stdev_tol(1) = std(times);
+fid(1) = Fidelity(rtot,r);
 
 [cnot,cz,zgate,hadgate] = ChangeTol(1e-6,cnot,cz,zgate,hadgate);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [r,p] = Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
     times(i) = toc;
 end
 t_tol(2) = mean(times);
 stdev_tol(2) = std(times);
-
+fid(2) = Fidelity(rtot,r);
 [cnot,cz,zgate,hadgate] = ChangeTol(1e-7,cnot,cz,zgate,hadgate);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
-    times(i) = toc;
+    [r,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
 end
 t_tol(3) = mean(times);
 stdev_tol(3) = std(times);
-
+fid(3) = Fidelity(rtot,r);
 [cnot,cz,zgate,hadgate] = ChangeTol(1e-8,cnot,cz,zgate,hadgate);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [r,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
     times(i) = toc;
 end
 t_tol(4) = mean(times);
 stdev_tol(4) = std(times);
-
+fid(4) = Fidelity(rtot,r);
 [cnot,cz,zgate,hadgate] = ChangeTol(1e-9,cnot,cz,zgate,hadgate);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [r,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
     times(i) = toc;
 end
 t_tol(5) = mean(times);
 stdev_tol(5) = std(times);
-
+fid(5) = Fidelity(rtot,r);
 [cnot,cz,zgate,hadgate] = ChangeTol(1e-10,cnot,cz,zgate,hadgate);
 for i = 1:reps
     tic;
-    Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
+    [r,p]=Correct_steane_error(rho,1,S,'X',0,0,hadgate,cnot,zgate,cz);
     times(i) = toc;
 end
 t_tol(6) = mean(times);
 stdev_tol(6) = std(times);
+fid(6) = Fidelity(rtot,r);
+
+%% test x gates
+
