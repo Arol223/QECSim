58d241ac2973fcd203ecd0a20661face2d2c31ea
Arvid Rolander <arvid.rolander@gmail.com>
2020-05-13 11:22:42 +0200

    Added rough description

--- Operations/Processes/SteaneLogicalGate.m
+++ Operations/Processes/SteaneLogicalGate.m
@@ -1,5 +1,6 @@
 function [rho] = SteaneLogicalGate(rho,gate, trgt_blk)
-%STEANELOGICALGATE Summary of this function goes here
+%STEANELOGICALGATE Performs a logical single qubit gate on a steane code
+%state
 %   Detailed explanation goes here
 targets = 1:7 + (trgt_blk-1)*7; % shift targets to target right logical bit. 
 rho = gate.apply(rho,targets);
