ff5a5f8d951b233d9ec66bd62c9a517ef7f12c3f
Arvid Rolander <arvid.rolander@gmail.com>
2021-04-23 16:26:52 +0200

    Surface17 results added

--- SimulationsNewProject/Flag_threshold.m
+++ SimulationsNewProject/Flag_threshold.m
@@ -1,7 +1,7 @@
 clear
 
-[cnot,cz,xgate,ygate,zgate,hadgate] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
-p_err = logspace(-9,-3,5);
+[cnot,cz,xgate,ygate,zgate,had] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
+p_err = logspace(-8,-2,6);
 ngates = 0;
 %% Physical gate
 rho_p = NbitState([1 0;0 0]); %p for physical
@@ -32,16 +32,16 @@ rho_l.set_e_init(0);
 fid_l = zeros(1,length(p_err));
 fid_EC = fid_l;
 parfor i = 1:length(p_err)
-    [cnot,cz,xgate,ygate,zgate,hadgate] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
+    [cnot,cz,xgate,ygate,zgate,had] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
     p = p_err(i);
     xgate.set_err(p,p);
     cnot.set_err(4*p,4*p);
     zgate.set_err(p,p);
-    hadgate.set_err(p,p);
+    had.set_err(p,p);
     rho_l.set_e_ro(p);
     rho_l.set_e_init(p);
     [cnot,cz] = SetDampCoeff(p,0, cnot, cz); % Sets damping for 2QBG
-    [xgate, zgate,ygate, hadgate] = SetDampCoeff(p,0, xgate, zgate,ygate, hadgate);
+    [xgate, zgate,ygate, had] = SetDampCoeff(p,0, xgate, zgate,ygate, had);
     
     
     %rtmp = SteaneLogicalGate(rho_l,xgate,1);
@@ -53,8 +53,8 @@ parfor i = 1:length(p_err)
         psi_tmp = SteaneLogicalGate(psi_tmp,xgate,1);
     end
     fid_l(i) =1- Fid2(psi_tmp,rtmp);
-    [rtmp,ptmp1] = FullFlagCorrection(rtmp,1,'X',cnot,hadgate,zgate,xgate);
-    [rtmp,ptmp2] = FullFlagCorrection(rtmp,1,'Z',cnot,hadgate,xgate,zgate);
+    [rtmp,ptmp1] = FullFlagCorrection(rtmp,1,'X',cnot,had,zgate,xgate);
+    [rtmp,ptmp2] = FullFlagCorrection(rtmp,1,'Z',cnot,had,xgate,zgate);
     fid_EC(i) = 1-Fid2(psi_tmp,rtmp);
 end
 
@@ -68,19 +68,19 @@ rho_l.set_e_init(0);
 fid_l = zeros(1,length(p_err));
 fid_EC_shor = fid_l;
 parfor i = 1:length(p_err)
-    [cnot,cz,xgate,~,zgate,hadgate] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
+    [cnot,cz,xgate,~,zgate,had] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
     p = p_err(i);
     xgate.set_err(p,p);
     cnot.set_err(4*p,4*p);
     cz.set_err(4*p,4*p);
     zgate.set_err(p,p);
-    hadgate.set_err(p,p);
+    had.set_err(p,p);
     rho_l.set_e_ro(p);
     rho_l.set_e_init(p);
 
     
     [cnot,cz] = SetDampCoeff(p,0, cnot, cz); % Sets damping for 2QBG
-    [xgate, zgate, hadgate] = SetDampCoeff(p,0, xgate, zgate, hadgate);
+    [xgate, zgate, had] = SetDampCoeff(p,0, xgate, zgate, had);
     rtmp = rho_l;
     psi_tmp = psi_l;
     for j = 2:ngates
@@ -88,8 +88,8 @@ parfor i = 1:length(p_err)
         psi_tmp = SteaneLogicalGate(psi_tmp,xgate,1);
     end
     fid_l(i) =1- Fid2(psi_tmp,rtmp);
-    rtmp = Correct_steane_error(rtmp,1,'X',p,p,hadgate,cnot,zgate,cz);
-    rtmp = Correct_steane_error(rtmp,1,'Z',p,p,hadgate,cnot,xgate,cz);
+    rtmp = Correct_steane_error(rtmp,1,'X',p,p,had,cnot,zgate,cz);
+    rtmp = Correct_steane_error(rtmp,1,'Z',p,p,had,cnot,xgate,cz);
     fid_EC_shor(i) = 1-Fid2(psi_tmp,rtmp);
 end
 
@@ -104,19 +104,19 @@ rho_l.set_e_init(0);
 fid_l_fivebit = zeros(1,length(p_err));
 fid_EC_fivebit = fid_l;
 for i = 1:length(p_err)
-    [cnot,cz,xgate,ygate,zgate,hadgate] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
+    [cnot,cz,xgate,ygate,zgate,had] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
     p = p_err(i);
     xgate.set_err(p,p);
     cnot.set_err(4*p,4*p);
     cz.set_err(4*p,4*p);
     zgate.set_err(p,p);
-    hadgate.set_err(p,p);
+    had.set_err(p,p);
     rho_l.set_e_ro(p);
     rho_l.set_e_init(p);
 
     
     [cnot,cz] = SetDampCoeff(p,0, cnot, cz); % Sets damping for 2QBG
-    [xgate, zgate, ygate,hadgate] = SetDampCoeff(p,0, xgate, zgate,ygate, hadgate);
+    [xgate, zgate, ygate,had] = SetDampCoeff(p,0, xgate, zgate,ygate, had);
     rtmp = rho_l;
     psi_tmp = psi_l;
     for j = 1:ngates
@@ -124,9 +124,49 @@ for i = 1:length(p_err)
         psi_tmp = xgate.apply(psi_tmp,1:5);
     end
     fid_l_fivebit(i) =1- Fid2(psi_tmp,rtmp);
-    rtmp = CorrectError(rtmp,1,cnot,hadgate,zgate,xgate,ygate);
+    rtmp = CorrectError(rtmp,1,cnot,had,zgate,xgate,ygate);
     fid_EC_fivebit(i) = 1-Fid2(psi_tmp,rtmp);
 end
+
+
+%% Surface17
+
+
+
+fid_l_surf = zeros(1,length(p_err));
+fid_EC_surf = fid_l_surf;
+parfor i = 1:length(p_err)
+    [psi_l, rho_l] = Logical0Surf17();
+    rho_l.set_t_ro(0); % Set readout time for state
+    rho_l.set_t_init(0) % Set initialisation time for ancillas
+    rho_l.set_e_ro(0);
+    rho_l.set_e_init(0);
+    
+    [cnot,cz,xgate,ygate,zgate,had] = MakeGates(0,0,[7.7e-6*[1 1] 3.36e-6*[1 1 1 1]],0,0); %Gate objects
+    p = p_err(i);
+    xgate.set_err(p,p);
+    cnot.set_err(4*p,4*p);
+    cz.set_err(4*p,4*p);
+    zgate.set_err(p,p);
+    had.set_err(p,p);
+    rho_l.set_e_ro(p);
+    rho_l.set_e_init(p);
+
+    
+    [cnot,cz] = SetDampCoeff(p,0, cnot, cz); % Sets damping for 2QBG
+    [xgate, zgate, ygate,had] = SetDampCoeff(p,0, xgate, zgate,ygate, had);
+    rtmp = rho_l;
+    psi_tmp = psi_l;
+    for j = 1:ngates
+        rtmp = xgate.apply(rtmp,[3 5 7]);
+        psi_tmp = xgate.apply(psi_tmp,[3 5 7]);
+    end
+    fid_l_surf(i) =1- Fid2(psi_tmp,rtmp);
+    [rtmp,~] = CorrectionCycle(rtmp,'X',cnot,had,xgate,zgate);
+    [rtmp,~] = CorrectionCycle(rtmp,'Z',cnot,had,xgate,zgate);
+    fid_EC_surf(i) = 1-Fid2(psi_tmp,rtmp);
+end
+
 
 %% Plotting
 %loglog(p_err,fid_phys)
@@ -135,6 +175,7 @@ hold on
 loglog(p_err,fid_EC)
 loglog(p_err,fid_EC_shor)
 loglog(p_err,fid_EC_fivebit)
+loglog(p_err,fid_EC_surf)
 xlabel('Error rate')
 ylabel('\epsilon_{fid}')
 title('\epsilon_{fid} vs p_{err}, cnot error = 4p_{err}')
